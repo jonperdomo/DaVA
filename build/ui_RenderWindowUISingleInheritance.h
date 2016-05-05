7e40c75a4d517688b65e8eb06488880ca720637b
Jonathan Perdomo <perdomojonathan5@gmail.com>
2016-05-04 21:03:33 -0400

    Initial commit-UI.

---
+++ build/ui_RenderWindowUISingleInheritance.h
@@ -0,0 +1,128 @@
+/********************************************************************************
+** Form generated from reading UI file 'RenderWindowUISingleInheritance.ui'
+**
+** Created by: Qt User Interface Compiler version 4.8.6
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_RENDERWINDOWUISINGLEINHERITANCE_H
+#define UI_RENDERWINDOWUISINGLEINHERITANCE_H
+
+#include <QtCore/QVariant>
+#include <QtGui/QAction>
+#include <QtGui/QApplication>
+#include <QtGui/QButtonGroup>
+#include <QtGui/QComboBox>
+#include <QtGui/QHBoxLayout>
+#include <QtGui/QHeaderView>
+#include <QtGui/QMainWindow>
+#include <QtGui/QMenu>
+#include <QtGui/QMenuBar>
+#include <QtGui/QPushButton>
+#include <QtGui/QStatusBar>
+#include <QtGui/QTextEdit>
+#include <QtGui/QVBoxLayout>
+#include <QtGui/QWidget>
+#include "QVTKWidget.h"
+
+QT_BEGIN_NAMESPACE
+
+class Ui_RenderWindowUISingleInheritance
+{
+public:
+    QAction *actionOpen_Data_Set;
+    QWidget *centralwidget;
+    QHBoxLayout *horizontalLayout_2;
+    QHBoxLayout *horizontalLayout;
+    QVBoxLayout *verticalLayout;
+    QComboBox *comboBox;
+    QPushButton *pushButton;
+    QTextEdit *textEdit;
+    QVTKWidget *qvtkWidget;
+    QMenuBar *menubar;
+    QMenu *menuFile;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *RenderWindowUISingleInheritance)
+    {
+        if (RenderWindowUISingleInheritance->objectName().isEmpty())
+            RenderWindowUISingleInheritance->setObjectName(QString::fromUtf8("RenderWindowUISingleInheritance"));
+        RenderWindowUISingleInheritance->resize(860, 604);
+        actionOpen_Data_Set = new QAction(RenderWindowUISingleInheritance);
+        actionOpen_Data_Set->setObjectName(QString::fromUtf8("actionOpen_Data_Set"));
+        centralwidget = new QWidget(RenderWindowUISingleInheritance);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        horizontalLayout_2 = new QHBoxLayout(centralwidget);
+        horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
+        horizontalLayout = new QHBoxLayout();
+        horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
+        verticalLayout = new QVBoxLayout();
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        comboBox = new QComboBox(centralwidget);
+        comboBox->setObjectName(QString::fromUtf8("comboBox"));
+
+        verticalLayout->addWidget(comboBox);
+
+        pushButton = new QPushButton(centralwidget);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+
+        verticalLayout->addWidget(pushButton);
+
+        textEdit = new QTextEdit(centralwidget);
+        textEdit->setObjectName(QString::fromUtf8("textEdit"));
+        QSizePolicy sizePolicy(QSizePolicy::Preferred, QSizePolicy::Preferred);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(textEdit->sizePolicy().hasHeightForWidth());
+        textEdit->setSizePolicy(sizePolicy);
+
+        verticalLayout->addWidget(textEdit);
+
+
+        horizontalLayout->addLayout(verticalLayout);
+
+        qvtkWidget = new QVTKWidget(centralwidget);
+        qvtkWidget->setObjectName(QString::fromUtf8("qvtkWidget"));
+
+        horizontalLayout->addWidget(qvtkWidget);
+
+
+        horizontalLayout_2->addLayout(horizontalLayout);
+
+        RenderWindowUISingleInheritance->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(RenderWindowUISingleInheritance);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 860, 21));
+        menuFile = new QMenu(menubar);
+        menuFile->setObjectName(QString::fromUtf8("menuFile"));
+        RenderWindowUISingleInheritance->setMenuBar(menubar);
+        statusbar = new QStatusBar(RenderWindowUISingleInheritance);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        RenderWindowUISingleInheritance->setStatusBar(statusbar);
+
+        menubar->addAction(menuFile->menuAction());
+        menuFile->addAction(actionOpen_Data_Set);
+
+        retranslateUi(RenderWindowUISingleInheritance);
+
+        QMetaObject::connectSlotsByName(RenderWindowUISingleInheritance);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *RenderWindowUISingleInheritance)
+    {
+        RenderWindowUISingleInheritance->setWindowTitle(QApplication::translate("RenderWindowUISingleInheritance", "HeelStat", 0, QApplication::UnicodeUTF8));
+        actionOpen_Data_Set->setText(QApplication::translate("RenderWindowUISingleInheritance", "Open Data Set", 0, QApplication::UnicodeUTF8));
+        pushButton->setText(QApplication::translate("RenderWindowUISingleInheritance", "Show Data Set", 0, QApplication::UnicodeUTF8));
+        menuFile->setTitle(QApplication::translate("RenderWindowUISingleInheritance", "File", 0, QApplication::UnicodeUTF8));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class RenderWindowUISingleInheritance: public Ui_RenderWindowUISingleInheritance {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_RENDERWINDOWUISINGLEINHERITANCE_H
